8da6c49c309a318805928271682815b06d1d2627
Danveer Sharma <danveer.sharma@startv.com>
2022-11-01 19:15:57 +0530

    Added linked list solutions

--- src/main/scala/programs/p0_chararter_count.scala
+++ src/main/scala/programs/p0_chararter_count.scala
@@ -7,6 +7,7 @@ package programs
 object p0_chararter_count extends App {
 
   def getCharCount(l:List[String]):Map[Char,Int] = {
+    //l.mkString("").split("").groupBy(_.charAt(0)).map(x=>x._1->x._2.size)
     val char_map = l.mkString("").map(char=>(char,1)) // create one single string and map 1 to each char
     val grouped = char_map.groupBy(_._1)              // group over key which is each char (value will have array of tuple having same char and 1)
     val res = grouped.mapValues(_.size).toMap         // foreach key( or char) find length of array
