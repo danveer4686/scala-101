09b314da05438bf78590a4945386f89b44f6e24c
Danveer Sharma <danveer.sharma@startv.com>
2022-12-18 22:11:15 +0530

    Feat: Add binary search programs

--- src/main/scala/programs/part2_binary_search/p5_find_peak_in_mountain_arr.scala
+++ src/main/scala/programs/part2_binary_search/p5_find_peak_in_mountain_arr.scala
@@ -6,17 +6,15 @@ Use binary search when array is sorted
 
 object p5_find_peak_in_mountain_arr extends App {
 
-  def findInMountain(nums:Array[Int]):Int={
-    var (start,end) = (0,nums.length-1)
+  def peakIndexInMountainArray(arr: Array[Int]): Int = {
+    var ( start, end, mid ) = (0, arr.length-1, -1)
     while(start<=end){
-      val mid = start+(end-start)/2
-      if(nums(mid)>nums(mid+1)) {
-        if(nums(mid)>nums(mid-1)) return mid
-          end = mid-1
-      }
-      else if (nums(mid)<nums(mid+1)) start = mid+1
+      mid = start + (end-start)/2
+      if (arr(mid) > arr(mid+1) && arr(mid) > arr(mid-1)) return mid
+      else if (arr(mid+1) > arr(mid))  start = mid+1
+      else end = mid-1
     }
-    -1
+    mid
   }
 
   def findInMountain2(nums:Array[Int]):Int={
