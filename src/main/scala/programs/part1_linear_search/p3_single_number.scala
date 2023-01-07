e1fbd6d413b0c664dae01edb491719eb97c4b5a2
danveer4686 <danveer4686@gmail.com>
2023-01-07 16:58:10 +0530

    Fix: Add single number

---
+++ src/main/scala/programs/part1_linear_search/p3_single_number.scala
@@ -0,0 +1,9 @@
+package programs.part1_linear_search
+object p3_single_number extends App {
+
+  def singleNumber(nums: Array[Int]): Int = {
+    nums.map(x=>(x,1)).groupBy(_._1).filter(_._2.size==1).keys.toList(0)
+  }
+
+  println(singleNumber(Array(2,2,1)))
+}
