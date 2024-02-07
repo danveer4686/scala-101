88759cbdb248a02aaf0ad525665f7a9a7b27e5bd
Danveer Sharma <danveer.sharma@startv.com>
2024-02-07 10:46:41 +0530

    Fix: Write more comprihensive example for sealed traits

--- src/main/scala/basics/part5bestpractices/SealedTraits.scala
+++ src/main/scala/basics/part5bestpractices/SealedTraits.scala
@@ -5,14 +5,14 @@ object SealedTraits extends App {
   //function with return type of sealed traits
   //guides user to consider all the possible outcomes at compile time
   sealed trait Result
-  case class Success(value: String) extends Result
-  case class InvalidInput(value: String, msg: String) extends Result
-  case class SubprocessFailed(return_code: Int, stderr: String) extends Result
+  case class Success(empName: String) extends Result
+  case class InvalidInput(return_code: Int, msg: String) extends Result
+  case class SubprocessFailed(return_code: Int, msg: String) extends Result
 
   def getEmpName(userId:Int): Result = {
     if (userId==13)                     SubprocessFailed(404, "Not Found")
     else if (userId<=100 && userId>=1)  Success("SomeName")
-    else                                InvalidInput("400", "BadInput")
+    else                                InvalidInput(400, "Bad Input")
   }
 
   def program():Unit ={
