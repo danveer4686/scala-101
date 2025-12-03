a342061a974dc60ef0757e5ae6a7a5a2fa016a40
Danveer Sharma <43093206+danveer4686@users.noreply.github.com>
2025-12-03 16:23:01 +0530

    Update p5_move_zeros_to_last.scala

--- src/main/scala/programs/part1_linear_search/p5_move_zeros_to_last.scala
+++ src/main/scala/programs/part1_linear_search/p5_move_zeros_to_last.scala
@@ -3,16 +3,19 @@ package programs.part1_linear_search
 object p5_move_zeros_to_last extends App{
 //https://leetcode.com/problems/move-zeroes/
   def moveZeroes(nums: Array[Int]): Unit = {
-    var (i,j) = (0,0)
-    while (j<nums.size){
-      if (nums(j)!=0) {
-        nums(i) = nums(j)
-        j=j+1
-        i=i+1
-      }
-      else j=j+1
+        var (i,j)=(0,0)
+        while (i<=nums.size-1){
+            if(nums(i)!=0) {
+                nums(j)=nums(i)
+                i=i+1
+                j=j+1
+            }
+            else i=i+1
+        }
+        while (j<nums.size){
+            nums(j)=0
+            j=j+1
+        }
     }
-    while (i< nums.size) nums(i) = 0
-  }
   moveZeroes(Array(0,2,0,1,4,5,0))
 }
